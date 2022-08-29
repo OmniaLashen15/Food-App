47e0abdaf8c33f912d71bf59672068dd911b9f34
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-29 15:17:50 +0200

    making modal for ordered items

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,14 +1,16 @@
 import classes from './Input.module.css';
+import React from 'react';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
             <input id={props.input.id} 
             {...props.input} //ensure that other attributes is set as id
+            ref={ref}
             />
         </div>
     );
-};
+});
 
 export default Input;
