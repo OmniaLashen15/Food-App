a165ac9dc161f1671b7ad3f5766b3b98e6f95b1d
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-29 16:50:07 +0200

    make add/remove functionality and using bump style

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,18 +2,33 @@ import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import { useContext } from 'react';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    
     const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({...item, amount:1});
+    };
     
     const cartItems = (
     <ul className={classes['cart-items']}>{
         cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+            <CartItem 
+            key={item.id} 
+            name={item.name} 
+            amount={item.amount} 
+            price={item.price} 
+            onRemove={cartItemRemoveHandler.bind(null,item.id)} // ensure that the id to be added or removed is passed here 
+            onAdd={cartItemAddHandler.bind(null,item)} //bind use for pre-configure the function
+            />
         ))}
         </ul>
         );
