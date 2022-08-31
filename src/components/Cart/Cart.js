806f0ac609c6344599cc957a0e8ea83176a2bf5d
OmniaLashen15 <omnia.lashen15@gmail.com>
2022-08-31 22:32:53 +0200

    add form validation for checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,62 +1,69 @@
-import CartContext from '../../store/cart-context';
+import { useContext, useState } from 'react';
+
 import Modal from '../UI/Modal';
-import classes from './Cart.module.css';
-import { useContext , useState} from 'react';
 import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 import Checkout from './Checkout';
 
 const Cart = (props) => {
-    const [isCheckout, setIsCheckout] = useState(false);
-    const cartCtx = useContext(CartContext);
+  const [isCheckout, setIsCheckout] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-    const hasItems = cartCtx.items.length > 0;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = (id) => {
-        cartCtx.removeItem(id);
-    };
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-    const cartItemAddHandler = (item) => {
-        cartCtx.addItem({...item, amount:1});
-    };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
-    const orderHandler = () => {
-        setIsCheckout(true);
-    };
-    
-    const cartItems = (
-    <ul className={classes['cart-items']}>{
-        cartCtx.items.map((item) => (
-            <CartItem 
-            key={item.id} 
-            name={item.name} 
-            amount={item.amount} 
-            price={item.price} 
-            onRemove={cartItemRemoveHandler.bind(null,item.id)} // ensure that the id to be added or removed is passed here 
-            onAdd={cartItemAddHandler.bind(null,item)} //bind use for pre-configure the function
-            />
-        ))}
-        </ul>
-        );
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
 
-    const modalActions = (
-        <div className={classes.actions}>
-        <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
-        {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}            
-        </div>
-    )
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
 
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            {isCheckout && <Checkout onCancel={props.onClose}/>}
-            {!isCheckout && modalActions }
-        </Modal>
-    );
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {!isCheckout && modalActions}
+    </Modal>
+  );
 };
 
 export default Cart;
